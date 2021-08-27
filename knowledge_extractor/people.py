ed73977bf3a92301562b4dc880c0569c19456ab0
KyeFelton <kye.felton@hotmail.com>
2021-08-27 19:14:00 +1000

    commented out sections in people.py

--- knowledge_extractor/people.py
+++ knowledge_extractor/people.py
@@ -2,59 +2,6 @@ import json
 
 CONTEXT = 'http://www.semanticweb.org/kyefelton/ontologies/2021/4/usyd-ontology',
 
-def entity(type, label):
-
-
-def copy_elements(json, keys):
-    new_json = {}
-    for key in keys:
-        new_json[key] = json[key]
-    return new_json
-
-def get_hr_details(person):
-    details = {}
-    keys = [
-        'fullName',
-        'givenName',
-        'surname',
-        'salutation',
-        'jobTitle',
-        'emailAddress',
-        'postalAddress',
-        'phoneNumber',
-        'faxNumber',
-        'mobilePhoneNumber'
-    ]
-    details.update(copy_elements(person, keys))
-    return details
-
-
-
-def get_affiliations(person):
-    primary_aff = {}
-    primary_aff_str = person['primaryFacultyAffiliation']
-    primary_aff_list = primary_aff_str.split(' > ')
-    faculty = {
-        '@type': 'Faculty',
-        'name': primary_aff_list[0],
-        'school': primary_aff_list[1]
-    }
-    school = {
-        '@type': 'School',
-        'name': primary_aff_list[1]
-    }
-    if primary_aff_list[1] != primary_aff_list[2]:
-        institute = {
-            '@type': 'Institute',
-            'name': primary_aff_list[2]
-        }
-        school['institute'] = primary_aff_list[2]
-        primary_aff = institute
-    else:
-        primary_aff = school
-
-    return primary_aff
-
 if __name__ == '__main__':
 
     with open('./people.json') as f:
@@ -117,3 +64,56 @@ if __name__ == '__main__':
         # keys = ['fullName', 'givenName', 'surname', 'salutation', 'emailAddress', ]
         # new_person = (person['getHrPerson'], keys)
         # print(new_person)
+
+#         def entity(type, label):
+
+
+# def copy_elements(json, keys):
+#     new_json = {}
+#     for key in keys:
+#         new_json[key] = json[key]
+#     return new_json
+
+# def get_hr_details(person):
+#     details = {}
+#     keys = [
+#         'fullName',
+#         'givenName',
+#         'surname',
+#         'salutation',
+#         'jobTitle',
+#         'emailAddress',
+#         'postalAddress',
+#         'phoneNumber',
+#         'faxNumber',
+#         'mobilePhoneNumber'
+#     ]
+#     details.update(copy_elements(person, keys))
+#     return details
+
+
+
+# def get_affiliations(person):
+#     primary_aff = {}
+#     primary_aff_str = person['primaryFacultyAffiliation']
+#     primary_aff_list = primary_aff_str.split(' > ')
+#     faculty = {
+#         '@type': 'Faculty',
+#         'name': primary_aff_list[0],
+#         'school': primary_aff_list[1]
+#     }
+#     school = {
+#         '@type': 'School',
+#         'name': primary_aff_list[1]
+#     }
+#     if primary_aff_list[1] != primary_aff_list[2]:
+#         institute = {
+#             '@type': 'Institute',
+#             'name': primary_aff_list[2]
+#         }
+#         school['institute'] = primary_aff_list[2]
+#         primary_aff = institute
+#     else:
+#         primary_aff = school
+
+#     return primary_aff
